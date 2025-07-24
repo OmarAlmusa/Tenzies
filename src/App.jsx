320fbfd6a36a2d302b86facaf479ec3ba859928e
Omar Almusa <omaralmusa01@gmail.com>
2025-07-25 02:07:04 +0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,47 @@
+import { useState } from "react";
 import Dice from "./Components/Dice"
 
-function App() {
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
-  const groupOfDices = Array.from({length: 10}, (_, i) => (
-    <Dice key={i} />
-  ))
+function App() {
+  const [diceList, setDiceList] = useState(() => 
+    Array.from({length: 10}, () => ({
+      value: getRandomInt(1, 6),
+      isClicked: false
+    }))
+  )
+
+  const nDicesClicked = diceList.filter(d => d.isClicked).length;
+
+  const handleClick = (idx) => {
+    setDiceList((prev) => 
+      prev.map((dice, i) => 
+        i === idx ? {...dice, isClicked: !dice.isClicked} : dice
+      )
+    );
+  }
+
+  const handleRoll = () => {
+
+    if(nDicesClicked===10){
+      const value_for_comparison = diceList[0].value;
+      const same_values = diceList.filter(d => d.value === value_for_comparison).length;
+
+      (same_values === 10) ? console.log("🎉 You win!") : console.log("❌ You lose!");
+    }
+
+    setDiceList((prev)=>
+      prev.map((dice) => 
+        dice.isClicked ? dice : {...dice, value: getRandomInt(1, 6)}
+      )
+    );
+
+    
+  };
 
   return (
     <>
@@ -13,10 +50,21 @@ function App() {
           <h1 className="font-bold text-4xl m-4">Tenzies</h1>
           <p className="text-center m-4">Roll until all dice are the same. Click each dice to freeze it at its current value between rolls.</p>
           <div className="grid grid-cols-5 gap-5 m-4">
-            {groupOfDices}
+            {diceList.map((dice, i) => (
+              <Dice 
+              key={i}
+              is_clicked={dice.isClicked}
+              handle_click_func={() => handleClick(i)}
+              dice_number={dice.value}
+              />
+            ))}
           </div>
-          <button className="bg-blue-600 text-white border-2 border-blue-400 rounded-xl px-6 py-2 m-4 text-lg hover:cursor-pointer hover:bg-blue-400 hover:scale-[105%] active:bg-blue-800 active:scale-[95%]">
-            Roll
+          <button 
+          className="bg-blue-600 text-white border-2 border-blue-400 rounded-xl px-6 py-2 m-4 text-lg 
+          hover:cursor-pointer hover:bg-blue-400 hover:scale-[105%] active:bg-blue-800 active:scale-[95%]"
+          onClick={handleRoll}
+          >
+            {(nDicesClicked === 10) ? "Check" : "Roll"}
           </button>
         </div>
       </div>
--- src/Components/Dice.jsx
+++ src/Components/Dice.jsx
@@ -1,20 +1,14 @@
-import { useState } from "react"
-
-function getRandomInt(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-export default function Dice(){
-
-    const [diceNumber, setDiceNumber] = useState(getRandomInt(1, 6))
+export default function Dice(props){
     return (
         <>
-            <button className="bg-white shadow-md rounded-xl box-border size-15 flex justify-center items-center font-medium text-lg
-                hover:scale-[105%] hover:cursor-pointer active:scale-[95%] active:bg-gray-100
-            ">
-                {diceNumber}
+            <button 
+            className={`
+            ${props["is_clicked"] ? "bg-green-400": "bg-white"} shadow-md rounded-xl box-border size-15 flex justify-center items-center font-medium text-lg 
+            hover:scale-[105%] hover:cursor-pointer active:scale-[95%] active:bg-gray-100
+            `}
+            onClick={props["handle_click_func"]}    
+            >
+                {props["dice_number"]}
             </button>
         </>
     )
